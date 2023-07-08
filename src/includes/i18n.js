70c5cd768be49a2386d65ac944238869cab42bfe
Denys Vladymyrov <link_amd@yahoo.com>
2023-07-09 01:31:30 +0300

    added i18n plugin for internationalization

---
+++ src/includes/i18n.js
@@ -0,0 +1,27 @@
+import { createI18n } from 'vue-i18n'
+import en from '@/locales/en.json'
+import fr from '@/locales/fr.json'
+
+export default createI18n({
+  locale: 'en',
+  fallbackLocale: 'en',
+  defaultScope: 'global',
+  messages: {
+    en,
+    fr
+  },
+  numberFormats: {
+    en: {
+      currency: {
+        style: 'currency',
+        currency: 'USD'
+      }
+    },
+    ja: {
+      currency: {
+        style: 'currency',
+        currency: 'JPY'
+      }
+    }
+  }
+})

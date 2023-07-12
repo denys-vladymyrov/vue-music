5371b3e718f2ba5729e456a6dee20759b3b2f545
Denys Vladymyrov <link_amd@yahoo.com>
2023-07-12 03:12:54 +0300

    installed vitest/ui and added 'sanity test'

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.12qll5ibvqg"
+    "revision": "0.ld8fsicq1hg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
